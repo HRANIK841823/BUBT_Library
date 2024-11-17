7cfd14029ecc743f9d6d154c7bf88b188bbe8bf8
HR Anik <anik841823.com>
2024-11-18 01:15:02 +0600

    host issue

--- Library_Management_Project/settings.py
+++ Library_Management_Project/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://bubt-library.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://bubt-library.onrender.com']
 
 # Application definition
 
